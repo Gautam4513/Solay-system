e00cefad880e1cfcf799f5317333d8e3fe39e334
Gautam Mandaviya <gautammandaviya21@gmail.com>
2024-10-11 16:25:37 +0530

    add animation

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import gsap from 'gsap';
 
 // Set up scene, camera, and renderer
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, 0.1, 100);
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('canvas'),
     antialias: true
@@ -12,31 +13,42 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// Add orbit controls
-const controls = new OrbitControls(camera, renderer.domElement);
+
 const spherProperties={
-    radius:1,
-    widthSegments:32,
+    radius:1.2,
+    widthSegments:64,
     heightSegments:32,
     color:[0x00ff00,0x0000ff,0xff0000,0x00ffff]
 }
+const oprbitRadius=4.3;
 const spheres=new THREE.Group();
-for(let i=0;i<=4;i++){
+for(let i=0;i<4;i++){
     const geometry = new THREE.SphereGeometry(spherProperties.radius,spherProperties.widthSegments,spherProperties.heightSegments);
     const material = new THREE.MeshBasicMaterial({ color: spherProperties.color[i]});
     const sphere = new THREE.Mesh(geometry, material);
+    const angle=(i/4)*(Math.PI*2);
+    sphere.position.x=oprbitRadius*Math.cos(angle);
+    sphere.position.z=oprbitRadius*Math.sin(angle);
     spheres.add(sphere);
 }
 scene.add(spheres)
 // Position camera
-camera.position.z = 5;
+spheres.rotation.x=0.13;
+spheres.position.y=-0.5;
+camera.position.z = 9;
 
 // Animation loop
 function animate() {
     requestAnimationFrame(animate);
-    controls.update();
     renderer.render(scene, camera);
 }
+// setInterval(()=>{
+//     gsap.to(spheres.rotation,{
+//         y:`+=${Math.PI/2}`,
+//         duration:2,
+//         ease:"expo.easeInOut",
+//     })
+// },3000)
 
 animate();
 
